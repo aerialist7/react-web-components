7ae649cc04418ee9e065ba7b847f6abec5a7f0e9
aerialist7 <aerialist7@github.com>
2022-01-22 14:03:52 +0400

    Cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    document.getElementById("root"),
-)
+const element = <React.StrictMode>
+    <App/>
+</React.StrictMode>
+
+const container = document.createElement("div")
+document.body.appendChild(container)
+
+ReactDOM.render(element, container)
